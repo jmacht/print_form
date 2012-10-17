66ed3aca17aa523d50dbee5d93c05524f7db36b1
Jeremiah Macht <jeremiahmacht@Jeremiah-Machts-MacBook-Pro.local>
2012-10-16 23:06:12 -0400

    committing print form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrintForm::Application.config.secret_token = 'a9344a640c93f5cdfcfd5b1212cf160830b020e5ec8a787183c5c94f11ccaacb02e2b20601659788b74ccc5b4b7fc92dd0261b7fd62deb8111685f7153cc0f33'
