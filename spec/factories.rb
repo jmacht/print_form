5caa76083e7068643b19292dc174a469e9d2c780
fmr09001 <frank.rende@uconn.edu>
2012-12-02 17:07:06 -0500

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Frank Rende"
+    email    "frank.rende@uconn.edu"
+    password "opim2012"
+    password_confirmation "opim2012"
+  end
+end
